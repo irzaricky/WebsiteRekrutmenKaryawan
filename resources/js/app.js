c38976720042d9755690591d680631793d751368
arsyirfza._ <irzasoul12@gmail.com>
2024-10-13 01:20:33 +0700

    mencoba vue bagian landing page

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,5 @@
-import './bootstrap';
+import "./bootstrap";
+import { createApp } from "vue";
+import app from "./components/LandingPage.vue";
+
+createApp(app).mount("#LandingPage");
--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -6,11 +6,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     @vite('resources/css/app.css')
-    <title>Document</title>
+    @vite(['resources/js/app.js'])
+    <title>Recrutier</title>
 </head>
 
 <body>
-
+    <div id="LandingPage"></div>
 </body>
 
 </html>
