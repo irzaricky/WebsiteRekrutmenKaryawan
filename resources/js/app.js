f61420b35fb4ac642968ca937ab916584fb29e86
arsyirfza._ <irzasoul12@gmail.com>
2024-10-16 14:27:29 +0700

    instalasi breeze vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,10 +1,27 @@
-import "./bootstrap";
-import "../css/app.css";
-import { createApp } from "vue";
-import router from "./router/router"; // Import the router configuration
-import App from "./App.vue"; // Import your main App.vue
+import '../css/app.css';
+import './bootstrap';
 
-// Create the app and use the router
-const app = createApp(App);
-app.use(router); // Use Vue Router in your app
-app.mount("#app"); // Mount the app to the main element in your HTML
+import { createInertiaApp } from '@inertiajs/vue3';
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+import { createApp, h } from 'vue';
+import { ZiggyVue } from '../../vendor/tightenco/ziggy';
+
+const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+
+createInertiaApp({
+    title: (title) => `${title} - ${appName}`,
+    resolve: (name) =>
+        resolvePageComponent(
+            `./Pages/${name}.vue`,
+            import.meta.glob('./Pages/**/*.vue'),
+        ),
+    setup({ el, App, props, plugin }) {
+        return createApp({ render: () => h(App, props) })
+            .use(plugin)
+            .use(ZiggyVue)
+            .mount(el);
+    },
+    progress: {
+        color: '#4B5563',
+    },
+});
