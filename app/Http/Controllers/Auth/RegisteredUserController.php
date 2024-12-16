3d420d1417d18e4ffa09e891a298cf3a6f2cd3d6
arsyirfza._ <irzasoul12@gmail.com>
2024-12-16 10:17:26 +0700

    fix redirect in RegisteredUserController; use intended route for redirection after user registration

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -75,6 +75,6 @@ class RegisteredUserController extends Controller
         event(new Registered($user));
         Auth::login($user);
 
-        return redirect(RouteServiceProvider::HOME);
+        return redirect()->intended(route('home'));
     }
 }
